131a4ee316b6a29219c794e1f7fa3e0fb3718684
Ubuntu <ubuntu@ip-172-31-87-149.ec2.internal>
2020-11-17 21:09:41 +0000

    added routes and changed message

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,2 +1,19 @@
-package io.pivotal.pal.tracker;public class PalTrackerApplication {
+package io.pivotal.pal.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// What are Spring Boot Features
+// - handles the creation of boiler plate code
+// - Auto Configuration
+// - dependency management
+// - web container added to "fat jar"
+
+
+@SpringBootApplication
+public class PalTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalTrackerApplication.class, args);
+    }
 }
